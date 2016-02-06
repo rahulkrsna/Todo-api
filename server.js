a4523c225dc0b319ed705671a802d78cc2d87cb9
Rahul Krishna <raahulkrsna@gmail.com>
2016-02-05 19:35:32 -0700

    New route added for todos

--- server.js
+++ server.js
@@ -2,12 +2,53 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 3000;
 var middleware = require("./middleware.js");
+var todos = [{
+	id: 1,
+	description: "Meet mom for lunch",
+	completed: false
+}, {
+	id: 2,
+	description: "Go to market",
+	completed: false
+}, {
+	id: 3,
+	description: "Purchase fruits",
+	completed: true
+}];
+
 
 app.use(middleware.logger);
 
 app.get("/", function(req, res) {
 	res.send('ToDo API Root');
 });
+
+// GET /todos
+app.get("/todos", function(req, res) {
+	res.json(todos); // This converts the array to json and send
+});
+
+// GET /todos
+app.get("/todos/:id", function(req, res) {
+	var todoId = parseInt(req.params.id,10);
+	var matchedTodo;
+	
+	// search all the todos for the todo with the id chosen.
+	todos.forEach(function (todo) {
+		console.log("id: " + todo.id + " "+todoId);
+		if (todo.id === todoId) {
+			matchedTodo = todo;
+		}
+	});
+	
+	// In case todo with the id is not found
+	if(matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send("Item not found");
+	}
+});
+
 
 app.listen(PORT, function() {
 	console.log("Express listening on port " + PORT +"!");
