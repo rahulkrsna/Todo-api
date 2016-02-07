8e24b479b66dc71a9dd6c9b0a8f9e2a7fd65eca4
Rahul Krishna <raahulkrsna@gmail.com>
2016-02-07 14:14:15 -0700

    Added Get with paramas(Filter)

---
+++ variable-playgroud.js
@@ -0,0 +1,24 @@
+//
+// var person = {
+// 	name: "Rahul",
+// 	age: 30
+// };
+//
+// function updatePerson(obj) {
+// 	obj.age = 31;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+
+// Array example
+
+var grades = [70,88];
+
+function addGrades(gradesArr) {
+	// gradesArr = [12,33];
+	gradesArr.push(90);
+	debugger;
+}
+addGrades(grades);
+console.log(grades);
