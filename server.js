8af120cec3d7bba63848aa3c011882294373bc71
Rahul Krishna <raahulkrsna@gmail.com>
2016-02-07 00:15:12 -0700

    Update(PUT) added

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.post("/todos", function(req, res) {
 	
 		res.json(body);
 	}
-	
 });
 
 //DELETE /todos/:id
@@ -66,6 +65,42 @@ app.delete("/todos/:id", function(req, res) {
 	} else {
 		res.status(404).send("Item not found");
 	}
+});
+
+// PUT /todos/:id
+app.put("/todos/:id", function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId})
+	var body = _.pick(req.body, "description", "completed");
+	var validAttributes = {};
+	
+	if(!matchedTodo) {
+		return res.status(404).send("Item not found");
+	}
+	
+	if (body.hasOwnProperty("completed")) {
+		if(_.isBoolean(body.completed)) {
+			validAttributes.completed = body.completed;
+		} else {
+			console.log("Not boolean");
+			return res.status(400).send();
+		}
+	}
+	if(body.hasOwnProperty("description")) {
+		if(_.isString(body.description) && body.description.trim().length > 0) {
+			validAttributes.description = body.description;	
+		} else {
+			console.log("Description not in format");
+			return res.status(400).send();
+		}
+	}
+	console.log("Updates: " + validAttributes);
+	// updated the attribute values in matchedTodo with that of validAttributes
+	console.log(matchedTodo);
+	_.extend(matchedTodo,validAttributes);
+	console.log(matchedTodo);
+	res.json(matchedTodo);
+	 
 });
 
 app.listen(PORT, function() {
